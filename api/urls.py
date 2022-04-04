a7582c550f88d74fc56e2482fb45fa3dd7d69f63
Marcos8060 <marcosgav80@gmail.com>
2022-04-05 01:37:34 +0300

    added the json web token for the user tokens and tokens refresh

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+
+
+# simple JWT url configurations
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns= [
    path('login/',views.LoginView.as_view()),
    path('branches/',views.BranchList.as_view()),
    path('orders/',views.OrderList.as_view()),
    path('orders/<int:pk>/',views.OrderDetail.as_view()),
+
+   # jwt routes
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from rest_framework import generics,status
 from .serializers import *
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 
 # Create your views here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['email'] = user.email
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
