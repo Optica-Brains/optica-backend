467f75d959b7850bd6e7445f79e76dfda7f913df
Moringa School <nobellanyarari@gmail.com>
2022-04-06 17:46:10 +0300

    Made changes to the models

--- api/models.py
+++ api/models.py
@@ -85,13 +85,21 @@ STATUS_CHOICES=(
 
 class Order(models.Model):
     order_number = models.CharField(max_length=250,null= False)
-    departure_time = models.DateTimeField(null = True)
-    delivery_time= models.DateTimeField(null = True)
-    status = models.CharField(max_length = 30,choices=STATUS_CHOICES,default='dispatched')
-    branch = models.ForeignKey(Branch,related_name='order_branch',on_delete=models.CASCADE, null=True)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.order_number
 
+
+class Batch(models.Model):
+    batch_number = models.CharField(max_length=30)
+    departure_time = models.DateTimeField(null = True)
+    delivery_time= models.DateTimeField(null = True)
+    status = models.CharField(max_length = 30,choices=STATUS_CHOICES,default='dispatched')
+    branch_from = models.ForeignKey(Branch,related_name='order_branch',on_delete=models.CASCADE, null=True)
+    branch_to = models.ForeignKey(Branch,related_name='order_branch',on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.batch_number
 
 
