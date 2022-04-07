764c29b97b824601d44dfdd34b64e68d44f37c3d
Moringa School <nobellanyarari@gmail.com>
2022-04-07 11:27:56 +0300

    Updated serializers and api

--- api/urls.py
+++ api/urls.py
@@ -11,9 +11,11 @@ from rest_framework_simplejwt.views import (
 urlpatterns= [
    path('create_user/',views.CreatUserView.as_view()),
    path('branches/',views.BranchList.as_view()),
-   path('orders/',views.OrderList.as_view()),
-   path('orders/<int:pk>/',views.OrderDetail.as_view()),
+   path('branches/<int:pk>/',views.BranchDetail.as_view()),
+   path('batches/',views.BatchesList.as_view()),
+   path('batches/<int:pk>/',views.BatchDetail.as_view()),
    path('users/',views.UsersView.as_view()),
+   path('users/<int:pk>/',views.UserDetails.as_view()),
 
 
    # jwt routes
